8f6f88b4e09f3d307a9cc467d18f588b8235d1a8
Fanttomas <97748713+Fanttomas@users.noreply.github.com>
2022-03-18 17:35:18 +0100

    Update fxmanifest in esx_skin

--- [esx]/esx_skin/fxmanifest.lua
+++ [esx]/esx_skin/fxmanifest.lua
@@ -6,36 +6,20 @@ description 'ESX Skin'
 
 version '1.6.0'
 
-shared_script '@es_extended/imports.lua'
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua'
+}
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/es.lua',
-	'locales/tr.lua',
-	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/sv.lua',
-	'locales/pl.lua',
-	'locales/es.lua',
-	'locales/tr.lua',
-	'config.lua',
 	'client/main.lua'
 }
 
